04221fca8facab75884e3fdf2a83b06a6f994ef2
joaobatistafrontend <jbbuno007@gmail.com>
2024-10-27 22:39:18 -0300

    alteracoes

---
+++ .history/core/urls_20241027220806.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
+    path('venda', NovaVendaView.as_view(), name='nova_venda'),
+    path('venda/finalizar', FinalizarVendaView.as_view(), name='finalizar_venda'),
+    path('venda/diminuir/<int:item_id>', DiminuirQuantidadeView.as_view(), name='diminuir_quantidade'),
+    path('venda/aumentar/<int:item_id>', AumentarQuantidadeView.as_view(), name='aumentar_quantidade'),
+    path('venda/delete/<int:item_id>', DeleteVenda.as_view(), name='delete_quantidade'),
+
+    
+    path('venda/editar/<int:pk>/', EditarVendaView.as_view(), name='editar_venda'),
+    path('venda/editar/<int:pk>/', EditarVendaView.as_view(), name='editar_venda'),
+
+]
